a73d8b11b79168315af150701664bc7af3529101
Vinay Yadav <vinayyadav91190@gmail.com>
2022-08-01 10:44:26 +0530

    for of loops in array with objects

---
+++ f12.js
@@ -0,0 +1,16 @@
+// objects inside array
+
+// very useful in real world applications
+
+const users = [
+    { userId: 1, firstName: "vinay", gender: "male" },
+    { userId: 2, firstName: "shivam", gender: "female" },
+    { userId: 3, firstName: "priyansh", gender: "male" },
+]
+
+// console.log(users);
+// ==============for of loops in array with objects
+// for (let user of users) {
+//     // console.log(user);  // for taking each object
+//     console.log(user.userId, user.firstName); // for taking each object userId
+// }
