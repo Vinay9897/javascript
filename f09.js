e215957b4ced758f90f049532cbb5411f9282487
Vinay Yadav <vinayyadav91190@gmail.com>
2022-08-01 09:45:55 +0530

    add key and value by using another variable

---
+++ f09.js
@@ -0,0 +1,10 @@
+//add key and value by using another variable
+
+const key = "email";
+const person = {
+    name: "Vinay",
+    age: 22
+}
+//add key with the name "email" 
+person[key] = "vinay.yadav_cs19@gla.ac.in";
+console.log(person);
