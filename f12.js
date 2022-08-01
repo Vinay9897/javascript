6e253bcc19f67440ba5ccc427966605e609c4bfe
Vinay Yadav <vinayyadav91190@gmail.com>
2022-08-01 10:53:16 +0530

    nested destructuring done

--- f12.js
+++ f12.js
@@ -14,3 +14,20 @@ const users = [
 //     // console.log(user);  // for taking each object
 //     console.log(user.userId, user.firstName); // for taking each object userId
 // }
+
+
+//nested destructuring
+
+// const [user1, user2, user3] = users;
+// console.log(user1);
+
+
+// first we destructure user1 from users then destructure firstName from user1
+// const [{ firstName }, user2, user3] = users;
+// console.log(firstName);
+
+// for name changing
+
+const [{ firstName: user1firstName, userId }, user2, user3] = users
+console.log(user1firstName);
+console.log(userId);
