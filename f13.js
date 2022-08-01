007cec1fc1a10d636906024e339559104b1bce23
Vinay Yadav <vinayyadav91190@gmail.com>
2022-08-01 11:40:19 +0530

    implement fun declaration, fun expression and arrow function

---
+++ f13.js
@@ -0,0 +1,59 @@
+// functions declaration
+
+function HappyBirthDay() {
+    console.log("Happy BirthDay");
+}
+
+HappyBirthDay();
+
+
+//function expersion
+const Number = function (x) {
+    if (x > 10)
+        return 1;
+    else
+        return 0;
+}
+
+// storing data in variables of function
+const data = Number(12);
+console.log(data);
+
+
+// Arrow function
+
+const HappyBirthDay1 = () => {
+    console.log("Happy BirthDay again");
+}
+
+HappyBirthDay1();
+
+// ####################################
+
+// ====== arrow function used with function expersion
+// const isEven = (number1, number2) => {
+//     if (number1 % 2 === 0)
+//         console.log("number1 is even");
+//     else
+//         console.log("number1 is odd");
+//     if (number2 % 2 === 0)
+//         console.log("number2 is even");
+//     else
+//         console.log("number2 is odd");
+// }
+
+// isEven(4, 5);
+
+// ####################################
+
+
+//==== modified  isEven() program 
+
+const isEven = number1 => number1 % 2 === 0;
+const ans = isEven(6);
+console.log(ans);
+
+
+
+
+
