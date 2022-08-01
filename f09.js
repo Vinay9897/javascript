50639bd0f777383ec6f66ed54a99b90bc564bf2e
Vinay Yadav <vinayyadav91190@gmail.com>
2022-08-01 09:55:32 +0530

    for in loops

--- f09.js
+++ f09.js
@@ -7,4 +7,23 @@ const person = {
 }
 //add key with the name "email" 
 person[key] = "vinay.yadav_cs19@gla.ac.in";
-console.log(person);
+// console.log(person);
+
+
+// loop with objects
+
+//for in loops
+
+for (let key in person) {
+    // console.log(key);
+    // console.log(person.key); // here we are checking string "key" name key
+    //  in whole object(3 keys) that's why 3 undefined
+    // console.log(person[key]);
+
+    // template string
+    // key-value pair with colon
+    // console.log(`${key} : ${person[key]}`)
+}
+
+
+console.log(typeof (Object.keys(person)));
