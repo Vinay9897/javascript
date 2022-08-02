783a0148841b776dd71e6ac7a584b5a33b0ae096
Vinay Yadav <vinayyadav91190@gmail.com>
2022-08-02 10:50:09 +0530

    implement function returning function

---
+++ f16.js
@@ -0,0 +1,13 @@
+// function returning function
+
+function fun1() {
+
+    function hello() {
+        // console.log("Hello vinay, i am define inside fun1 ");
+        return "hello world";
+    }
+    return hello;
+}
+
+const ans = fun1();
+console.log(ans());
