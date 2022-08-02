a533a9a3189133706c897fc09108239ef8788997
Vinay Yadav <vinayyadav91190@gmail.com>
2022-08-02 10:15:05 +0530

    implement rest parameter concept

---
+++ f14.js
@@ -0,0 +1,36 @@
+// function inside function
+
+// const app = () => {
+//     const addition = (number1, number2) =>
+//         number1 + number2;
+
+//     const mul = (num1, num2) => num1 * num2;
+
+//     const hello = () => console.log("Hello Vinay");
+
+//     hello();
+//     console.log(mul(3, 5));
+//     console.log(addition(3, 5));
+// }
+
+// app();
+
+
+// rest parameters (...c)
+
+// function myFunc(a, b, ...c) {
+//     console.log(`a is ${a}`);
+//     console.log(`b is ${b}`);
+//     console.log(`c is ${c}`);
+// }
+
+// myFunc(1, 2, 3, 4, 5, 6);
+
+// addAll = (...arr) => {
+//     let sum = 0;
+//     for (let x of arr) {
+//         sum += x;
+//     }
+//     return sum;
+// }
+// console.log(addAll(1, 2, 3, 4, 5));
