53829fca50d70933fb52ba556120cf9e7e8cbc2e
Vinay Yadav <vinayyadav91190@gmail.com>
2022-08-02 15:55:23 +0530

    use set

---
+++ f27.js
@@ -0,0 +1,48 @@
+//Set (it is iterable)
+// store data
+
+//set also have ites own methods
+// No index-based access
+// order is not guranted
+// unique items only (no duplicates allowed)
+
+const numbers = new Set([1, 2, 2, 3, 3, 5, 5, 6]);
+// console.log(numbers);
+
+// const numb = new Set();
+// numb.add(1);
+// numb.add(1);
+// numb.add(2);
+// numb.add(2);
+// numb.add(3);
+// numb.add(3);
+// numb.add(4);
+// numb.add(4);
+// numb.add(5);
+// numb.add(5);
+// console.log(numb);
+// if (numb.has(1))
+//     console.log("1 is present");
+// else
+//     console.log("1 is not present");
+
+const unique = new Set(numbers);
+console.log(unique);
+
+const data = new Set();
+data.add(
+    { "1": 'One' },
+    { 2: 'two' },
+
+);
+data.set({ 1: 'one' }, { 2: "tow" });
+data.set([1, 2, 3], 'array');
+
+
+
+
+
+
+
+
+
