f0f68ed75eaab6e717a54dc19cf776ef00730955
Vinay Yadav <vinayyadav91190@gmail.com>
2022-08-02 14:48:28 +0530

    what is iterable

---
+++ f26.js
@@ -0,0 +1,18 @@
+// iterable
+
+// iterable are those ,in which we can use loops
+// string ,array are iterable
+const firstName = "harshit";
+for (let char of firstName) {
+    console.log(char);
+}
+
+const items = ['item', 'items', 'items']
+for (let word of firstName) {
+    console.log(word);
+}
+
+// array like object
+//those have length property (ex :-array and string)
+// and those which we can access by index
+// example : string
