d5618f2ccca72bc7d6566f54ca3af1e75faf7253
Vinay Yadav <vinayyadav91190@gmail.com>
2022-08-03 19:05:06 +0530

    create Object using function prototype

--- f40.js
+++ f40.js
@@ -1,26 +1,28 @@
 // create new Object in function using function Object.create()
 
-const userMethods = {
-    about: function () {
-        return `${this.firstName}${this.lastName} is ${this.age} year old and his emailid is${this.email} and address ${this.address}`
-    },
-    is18: function () {
-        return this.age >= 18;
-    }
-}
+// const userMethods = {
+//     about: function () {
+//         return `${this.firstName}${this.lastName} is ${this.age} year old and his emailid is${this.email} and address ${this.address}`
+//     },
+//     is18: function () {
+//         return this.age >= 18;
+//     }
+// } 
 function createObject(firstName, lastName, email, age, address) {
-    const user = Object.create(userMethods);// object in which we store all the data
+    const user = Object.create(createObject.prototype);// object in which we store all the data
     //    key      =   value
     user.firstName = firstName;
     user.lastName = lastName;
     user.email = email;
     user.age = age;
-    // user.address = address;
-    // user.about = userMethods.about;
-    // user.is18 = userMethods.is18;
     return user;
 }
-
+createObject.prototype.about = function () {
+    return `${this.firstName}${this.lastName} is ${this.age} year old and his emailid is${this.email} and address ${this.address}`
+};
+createObject.prototype.is18 = function () {
+    return this.age >= 18;
+};
 const user1 = createObject("vinay", "yadav", " vinay.yadav_cs19@gla.ac.in ", 22, "kuldeep vihar");
 const user2 = createObject("vinay", "yadav", "vinay.yadav_cs19@gla.ac.in", 22, "kuldeep vihar");
 const user3 = createObject("vinay", "yadav", "vinay.yadav_cs19@gla.ac.in", 22, "kuldeep vihar");
