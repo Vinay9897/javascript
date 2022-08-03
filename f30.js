e182ea5bbc755c29587e4010b9b8b140317cf4dd
Vinay Yadav <vinayyadav91190@gmail.com>
2022-08-03 15:07:30 +0530

    implement Optonal Chaining concept

---
+++ f30.js
@@ -0,0 +1,14 @@
+//  Optional Chaining
+
+const obj = {
+    id: 1,
+    age: 23,
+    name: "vinay",
+    // hobbie: { anime: "Tokyo Ghoul" },
+}
+// this is chaining
+// console.log(obj.hobbie.anime);.
+
+// This is optional Chaining -- in which even data is not 
+// present in object still doesn't give error
+console.log(obj?.hobbie?.anime);
