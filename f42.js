62bd6ad5b33733e9c8e6a1b4d77599eac5386e4f
Vinay Yadav <vinayyadav91190@gmail.com>
2022-08-23 23:03:20 +0530

    learn about querySelector

--- f42.js
+++ f42.js
@@ -2,3 +2,11 @@
 const website = document.getElementById("website");
 const c = website.style.color = "blue";
 
+
+//querySelector is used for selecting properties(anyone)
+
+let mainHeading = document.querySelector("#main-heading");
+const heading = mainHeading.textContent = "MANAGE YOUR TASK";
+console.log(heading);
+
+//querySeclector 
