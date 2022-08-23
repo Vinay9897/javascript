ad26ecde7d12bf02358a4041c821b69d574579e8
Vinay Yadav <vinayyadav91190@gmail.com>
2022-08-23 23:47:19 +0530

    learn query selectorAll()

--- f42.js
+++ f42.js
@@ -5,8 +5,19 @@ const c = website.style.color = "blue";
 
 //querySelector is used for selecting properties(anyone)
 
+// id selector
 let mainHeading = document.querySelector("#main-heading");
 const heading = mainHeading.textContent = "MANAGE YOUR TASK";
 console.log(heading);
 
-//querySeclector 
+// class selector
+const navitems = document.querySelector(".nav-item");
+console.log(navitems)
+
+//querySeclectorAll is used for select all properties
+// it is a 
+
+const navites = document.querySelectorAll(".nav-item");
+console.log(navites)
+
+
